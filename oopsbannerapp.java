5140d82b95b36c69f79aba95396143ea74f310be
divyanshishishodia <shishodia.divyanshi25@gmail.com>
2026-03-03 10:10:20 +0530

    printing oops

--- oopsbannerapp.java
+++ oopsbannerapp.java
@@ -16,5 +16,19 @@ public class oopsbannerapp {
         System.out.println(String.join(" ", "*   *", "*   *", "*     ", "     *"));
         System.out.println(String.join(" ", "*   *", "*   *", "*     ", "*   *"));
         System.out.println(String.join(" ", " *** ", " *** ", "*     ", " *****"));
+		
+		String[] lines = {
+            String.join(" ", "  ***  ", "  ***  ", " ****** ", "  ***** "),
+            String.join(" ", " **   ** ", " **   ** ", " **    ** ", " **     "),
+            String.join(" ", "**     **", "**     **", " **    ** ", " **     "),
+            String.join(" ", "**     **", "**     **", " ****** ", "  ***** "),
+            String.join(" ", "**     **", "**     **", " **      ", "       **"),
+            String.join(" ", " **   ** ", " **   ** ", " **      ", " **    **"),
+            String.join(" ", "  ***  ", "  ***  ", " **      ", "  ***** ")
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
 	}
 }	
